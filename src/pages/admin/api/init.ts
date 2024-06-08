1e53148d9c335e251d03e692dab07053a9b08a3b
Javier Mota <javier.mota@edreamsodigeo.com>
2024-06-08 23:53:30 +0200

    fix db

--- src/pages/admin/api/init.ts
+++ src/pages/admin/api/init.ts
@@ -2,23 +2,18 @@ import { createClient } from "@vercel/postgres";
 
 export async function GET() {
   try {
-    // const client = createClient({
-    //   connectionString: import.meta.env.POSTGRES_URL,
-    // });
-    // const result = await client.sql`CREATE TABLE Guest (
-    //     id SERIAL PRIMARY KEY,
-    //     name VARCHAR(255) NOT NULL,
-    //     expected_attendees INTEGER NOT NULL,
-    //     confirmed_attendees INTEGER NOT NULL,
-    //     bus BOOLEAN NOT NULL,
-    //     allergies TEXT,
-    //     modified_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-    //   );`;
-    return new Response(
-      JSON.stringify({
-        result: import.meta.env.POSTGRES_URL,
-      })
-    );
+    const client = createClient({
+      connectionString: import.meta.env.POSTGRES_URL_NON_POOLING,
+    });
+    const result = await client.sql`CREATE TABLE Guest (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        expected_attendees INTEGER NOT NULL,
+        confirmed_attendees INTEGER NOT NULL,
+        bus BOOLEAN NOT NULL,
+        allergies TEXT,
+        modified_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+      );`;
   } catch (error) {
     return new Response(
       JSON.stringify({
--- src/repository/db.ts
+++ src/repository/db.ts
@@ -19,5 +19,5 @@ export type Guest = Selectable<GuestTable>;
 export type GuestInput = Insertable<GuestTable>;
 
 export default createKysely<Database>({
-  connectionString: import.meta.env.POSTGRES_URL,
+  connectionString: import.meta.env.POSTGRES_URL_NON_POOLING,
 });
