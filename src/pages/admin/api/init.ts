27b7d5d8a9dd71e68bb54fcd4e3ad842381b5ac7
Javier Mota <javier.mota@edreamsodigeo.com>
2024-06-08 23:50:21 +0200

    test db

--- src/pages/admin/api/init.ts
+++ src/pages/admin/api/init.ts
@@ -2,21 +2,21 @@ import { createClient } from "@vercel/postgres";
 
 export async function GET() {
   try {
-    const client = createClient({
-      connectionString: import.meta.env.POSTGRES_URL,
-    });
-    const result = await client.sql`CREATE TABLE Guest (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        expected_attendees INTEGER NOT NULL,
-        confirmed_attendees INTEGER NOT NULL,
-        bus BOOLEAN NOT NULL,
-        allergies TEXT,
-        modified_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-      );`;
+    // const client = createClient({
+    //   connectionString: import.meta.env.POSTGRES_URL,
+    // });
+    // const result = await client.sql`CREATE TABLE Guest (
+    //     id SERIAL PRIMARY KEY,
+    //     name VARCHAR(255) NOT NULL,
+    //     expected_attendees INTEGER NOT NULL,
+    //     confirmed_attendees INTEGER NOT NULL,
+    //     bus BOOLEAN NOT NULL,
+    //     allergies TEXT,
+    //     modified_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    //   );`;
     return new Response(
       JSON.stringify({
-        result,
+        result: import.meta.env.POSTGRES_URL,
       })
     );
   } catch (error) {
