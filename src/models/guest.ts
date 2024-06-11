886b1db7aaf31158ac5fa17cf3e4444bf4327108
Javier Mota <javier.mota@edreamsodigeo.com>
2024-06-11 17:14:29 +0200

    remove models folder

--- src/models/guest.ts
+++
@@ -1,9 +0,0 @@
-// src/models/guest.ts
-export interface Guest {
-	id: number;
-	name: string;
-	expectedAttendees: number;
-	confirmedAttendees: number;
-	bus: boolean;
-	allergies?: string;
-}
--- src/repository/db.ts
+++ src/repository/db.ts
@@ -5,25 +5,25 @@ import { SerializePlugin } from "kysely-plugin-serialize";
 import type { GuestTable } from "./model";
 
 interface Database {
-  guest: GuestTable;
+	guest: GuestTable;
 }
 
 const createProdDB = () =>
-  createKysely<Database>({
-    connectionString: import.meta.env.POSTGRES_URL,
-  });
+	createKysely<Database>({
+		connectionString: import.meta.env.POSTGRES_URL,
+	});
 
 const createDevDB = () => {
-  const dialect = new SqliteDialect({
-    database: new SQLite("local.db"),
-  });
-  return new Kysely<Database>({
-    dialect,
-    plugins: [new SerializePlugin()],
-  });
+	const dialect = new SqliteDialect({
+		database: new SQLite("local.db"),
+	});
+	return new Kysely<Database>({
+		dialect,
+		plugins: [new SerializePlugin()],
+	});
 };
 
 const db =
-  import.meta.env.MODE === "development" ? createDevDB() : createProdDB();
+	import.meta.env.MODE === "development" ? createDevDB() : createProdDB();
 
 export default db;
