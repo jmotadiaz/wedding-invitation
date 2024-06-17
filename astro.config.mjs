ccf9ec4ce35f0942d1794ab5baa52d031ac04090
Javier Mota <javier.mota@edreamsodigeo.com>
2024-06-17 12:13:35 +0200

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
-
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), alpinejs({ entrypoint: "/src/config/alpine" })],
 	output: "server",
-	adapter: vercel(),
+	adapter: vercel({
+		webAnalytics: true
+	}),
 });
